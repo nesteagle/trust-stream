b1efff01ed01b6ff63598debefcad6eaec485148
nesteagle <104323706+nesteagle@users.noreply.github.com>
2026-08-14 04:18:20 +0000

    Remove old imports from evaluator.py and add helper

--- app/pipeline/evaluator.py
+++ app/pipeline/evaluator.py
@@ -1,11 +1,10 @@
 import pandas as pd
 
-from app.evaluator.io import load_and_parse_eval, load_scores
-from app.evaluator.scoring import (
-    get_explanation_scores,
-    get_external_score,
-    get_internal_score,
-)
+from app.evaluator.io import load_and_parse_eval
+
+
+def _get_explanation_scores(explanation: dict, message_id: str) -> dict:
+    return explanation.get(message_id)
 
 
 def _get_eval_ids(message_id: str) -> tuple[str, str]:
@@ -18,9 +17,9 @@ def score_dataframe(df: pd.DataFrame, eval_path: str) -> pd.DataFrame:
     explanations = load_and_parse_eval(eval_path)
 
     df["score_explanations_external"] = df["message_id"].apply(
-        lambda m_id: get_explanation_scores(explanations, _get_eval_ids(m_id)[0])
+        lambda m_id: _get_explanation_scores(explanations, _get_eval_ids(m_id)[0])
     )
     df["score_explanations_internal"] = df["message_id"].apply(
-        lambda m_id: get_explanation_scores(explanations, _get_eval_ids(m_id)[1])
+        lambda m_id: _get_explanation_scores(explanations, _get_eval_ids(m_id)[1])
     )
     return df
